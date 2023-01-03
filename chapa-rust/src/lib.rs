468b331644663b0f9292d45ffed63b9780a766a4
whambamleo <89803624+whambamleo@users.noreply.github.com>
2023-01-03 11:46:56 -0600

    retrieving banks successfully (simple example in main)

--- chapa-rust/src/lib.rs
+++ chapa-rust/src/lib.rs
@@ -1,3 +1,4 @@
+// example function
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -6,6 +7,7 @@ pub fn add(left: usize, right: usize) -> usize {
 mod tests {
     use super::*;
 
+    // example test
     #[test]
     fn it_works() {
         let result = add(2, 2);
